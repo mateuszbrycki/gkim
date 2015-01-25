a57819aaf80b80c7c462a0759a891f87008f5747
Mateusz Brycki <mateusz.brycki@outlook.com>
2015-01-25 16:22:52 +0100

    Obsługa podstawowych zdarzeń w interfejsie użytkownika.

--- dreamTeam-beta/mainwindow.cpp
+++ dreamTeam-beta/mainwindow.cpp
@@ -1,11 +1,104 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QDebug>
+#include <QFileDialog>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+}
+
+QString MainWindow::getFilePath(int mode) {
+
+    QString selectedFilePath = QFileDialog::getOpenFileName(this,
+        tr("Otwórz obraz"), "", tr((mode == 0) ? "Image Files (*.bmp)" : "Image Files (*.dt)" ));
+
+    if(mode == 0) {
+        ui->bdFile->setMaxLength(this->qLineMaxLength);
+        ui->bdFile->setText(selectedFilePath);
+    } else {
+        ui->dbFile->setMaxLength(this->qLineMaxLength);
+        ui->dbFile->setText(selectedFilePath);
+    }
+    return selectedFilePath;
+}
+
+QString MainWindow::getFolderPath(int mode) {
+    QString selectedFolderPath =  QFileDialog::getExistingDirectory(this, tr("Wskaż folder"),
+         "", QFileDialog::ShowDirsOnly | QFileDialog::DontResolveSymlinks);
+
+    if(mode == 0) {
+        ui->bdPath->setMaxLength(this->qLineMaxLength);
+        ui->bdPath->setText(selectedFolderPath);
+    } else {
+        ui->dbPath->setMaxLength(this->qLineMaxLength);
+        ui->dbPath->setText(selectedFolderPath);
+    }
+    return selectedFolderPath ;
+}
+
+void MainWindow::on_bdFile_selectionChanged()
+{
+    if((ui->bdFile->text()).isEmpty()) {
+        QString filePath = this->getFilePath(0);
+    }
+}
+
+void MainWindow::on_bdFileButton_released()
+{
+    QString filePath = this->getFilePath(0);
+}
+
+void MainWindow::on_bdPath_selectionChanged()
+{
+    if((ui->bdPath->text()).isEmpty()) {
+        QString folderPath = this->getFolderPath(0);
+    }
+}
+
+void MainWindow::on_bdPathButton_released()
+{
+    QString folderPath = this->getFolderPath(0);
+}
+
+void MainWindow::on_bdConvertButton_released()
+{
+    if(!(ui->bdFile->text()).isEmpty() && !(ui->bdPath->text()).isEmpty()) {
+        qDebug()<<"Obsługa konwersji z BMP do DT";
+    }
+}
+
+void MainWindow::on_dbFile_selectionChanged()
+{
+    if((ui->dbFile->text()).isEmpty()) {
+        QString filePath = this->getFilePath(1);
+    }
+}
+
+void MainWindow::on_dbPath_selectionChanged()
+{
+    if((ui->dbPath->text()).isEmpty()) {
+        QString folderPath = this->getFolderPath(1);
+    }
+}
+
+void MainWindow::on_dbFileButton_released()
+{
+     QString filePath = this->getFilePath(1);
+}
+
+void MainWindow::on_dbPathButton_released()
+{
+     QString folderPath = this->getFolderPath(1);
+}
+
+void MainWindow::on_dbConvertButton_released()
+{
+    if(!(ui->dbFile->text()).isEmpty() && !(ui->dbPath->text()).isEmpty()) {
+        qDebug()<<"Obsługa konwersji z DT do BMP";
+    }
 }
 
 MainWindow::~MainWindow()
--- dreamTeam-beta/mainwindow.h
+++ dreamTeam-beta/mainwindow.h
@@ -15,8 +15,33 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+    void on_bdFile_selectionChanged();
+
+    void on_bdPath_selectionChanged();
+
+    void on_bdFileButton_released();
+
+    void on_bdPathButton_released();
+
+    void on_bdConvertButton_released();
+
+    void on_dbFile_selectionChanged();
+
+    void on_dbPath_selectionChanged();
+
+    void on_dbFileButton_released();
+
+    void on_dbPathButton_released();
+
+    void on_dbConvertButton_released();
+
 private:
     Ui::MainWindow *ui;
+    QString getFilePath(int mode);
+    QString getFolderPath(int mode);
+
+    int qLineMaxLength = 200;
 };
 
 #endif // MAINWINDOW_H
