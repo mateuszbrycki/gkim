3069c506e9ba7e2c2b21d7b8eeb9b969eb793cb5
bryzikm <bryzikm@gmail.com>
2015-01-26 16:14:51 +0100

    Poprawiona klasa Picture

--- coder/Picture.cpp
+++ coder/Picture.cpp
@@ -10,7 +10,9 @@ using namespace std;
 */
 Picture::Picture(string openPath, int colorType)
 {
-    SDL_Surface* BMP = SDL_LoadBMP(openPath);
+    this->colorType = colorType;
+
+    BMP = SDL_LoadBMP(openPath);
 
     if(!BMP) //sprawdzamy czy obrazek taki w ogóle istnieje
     {
@@ -18,14 +20,28 @@ Picture::Picture(string openPath, int colorType)
     }
 }
 /*
+** funkcja zwracajaca szerokosc obrazka
+*/
+int Picture::getPictureWidth()
+{
+    return (BMP->w);
+}
+/*
+** funkcja zwracajaca wysokosc obrazka
+*/
+int Picture::getPictureHeight()
+{
+    return (BMP->h);
+}
+/*
 ** Funkcja pobierajaca kolor danego piksela
 ** źródło: pliki z laboratorium z GKiM
 */
-SDL_Color getPixelColor(int x, int y)
+SDL_Color Picture::getPixelColor(int x, int y)
 {
     SDL_Color color ;
     Uint32 col = 0 ;
-    if ((x >= 0) && (x < BMP->getPictureWidth()) && (y >= 0) && (y < BMP->getPictureHeight())) {
+    if ((x >= 0) && (x < Picture.getPictureWidth()) && (y >= 0) && (y < Picture.getPictureHeight())) {
         //determine position
         char* pPosition=(char*)screen->pixels ;
         //offset by y
@@ -37,6 +53,15 @@ SDL_Color getPixelColor(int x, int y)
         //convert color
         SDL_GetRGB(col, screen->format, &color.r, &color.g, &color.b);
     }
+
+        if(colorType == 1)
+        {
+            color.r = 0.299*color.r + 0.587*color.g + 0.114*color.b;
+            color.g = 0.299*color.r + 0.587*color.g + 0.114*color.b;
+            color.b = 0.299*color.r + 0.587*color.g + 0.114*color.b;
+        }
+
+
     return ( color ) ;
 }
 /*
@@ -45,11 +70,11 @@ SDL_Color getPixelColor(int x, int y)
 list<SDL_Color> Picture::getPictureColors()
 {
     list<SDL_Color> ListOfColors;
-    bool IsIn = false;
+
     SDL_Color color;
 
-        int x = BMP->getPictureWidth(),
-            y = BMP->getPcitureHeight();
+        int x = Picture.getPictureWidth(),
+            y = Picture.getPictureHeight();
 
             for(int i = 0; i < x; i++)
             {
@@ -59,13 +84,14 @@ list<SDL_Color> Picture::getPictureColors()
                     if(!isInList(ListOfColors, color)) {
                         ListOfColors.push_back(color);
                     }
-
                 }
             }
 
       return ListOfColors;
 }
-
+/*
+** funkcja: sprawdza czy kolor został już wyszukany / czy znajduje sie w liście
+*/
 bool Picture::isInList(list<SDL_Color&> ListOfColors, SDL_Color& color) {
     for(list<SDL_Color>::iterator it = ListOfColors.begin(); it != ListOfColors.end(); it++)
     {
