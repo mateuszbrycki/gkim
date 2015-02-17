4aeded82ed1b69b70a6c3113dd9275871bce6ebf
Mateusz Brycki <mateusz.brycki@outlook.com>
2015-02-17 18:06:44 +0100

    Zamiana z zapisu binarnego na szesnastkowy, jeszcze nie dział¦a

--- dtConverter/FileWriter.cpp
+++ dtConverter/FileWriter.cpp
@@ -4,6 +4,7 @@
 #include <bitset>
 #include <math.h>
 #include <iostream>
+#include <iomanip>
 
 #include "FileWriter.h"
 
@@ -40,24 +41,23 @@ void FileWriter::saveFile(Picture *picture, const vector<int>& pixelListAfterCom
     ofstream file(savePath.c_str());
 
     //zapis nagłówka do pliku
-    file << bitset<24>(picture->getPictureWidth());
-    file << bitset<24>(picture->getPictureHeight());
-    int pixelWidth = (log(maxIndex) / log(2)) + 1; //tyle bitów potrzeba do zapisania każdego z użytych indeksów słownika;
-    file << bitset<24>(pixelWidth);
-    file << bitset<16>(105); //start słownika jest stały
-    int pictureStart = 105 + colorsList.size()*24;
-    file << bitset<16>(pictureStart);
+    file<<std::setfill('0')<<std::setw(6)<<std::hex<<picture->getPictureWidth();
+    file<<std::setfill('0')<<std::setw(6)<<std::hex<<picture->getPictureHeight();
+    int pixelWidth = (log(maxIndex) / log(16)) + 1; //tyle bitów potrzeba do zapisania każdego z użytych indeksów słownika;
+    file<<std::setfill('0')<<std::setw(6)<<std::hex<<pixelWidth;
+    file<<std::setfill('0')<<std::setw(4)<<std::hex<<23;
+    int pictureStart = 23 + colorsList.size()*6;
+    file<<std::setfill('0')<<std::setw(4)<<std::hex<<pictureStart;
 
     //zapis kolorów obrazka do pliku - słownik
     for(vector<SDL_Color>::const_iterator it = colorsList.begin(); it != colorsList.end(); ++it) {
-        file<<bitset<8>((*it).r) ;
-        file<<bitset<8>((*it).g);
-        file<<bitset<8>((*it).b);
+        file<<std::setfill('0')<<std::setw(2)<<std::hex<<((*it).r);
+        file<<std::setfill('0')<<std::setw(2)<<std::hex<<((*it).g);
+        file<<std::setfill('0')<<std::setw(2)<<std::hex<<((*it).b);
     }
     //zapis pikseli do pliku
     for(vector<int>::const_iterator it = pixelListAfterCompression.begin(); it != pixelListAfterCompression.end(); it++) {
-        //tutaj nie można użyć bitset
-        file<<this->convertValueToBinary(*it, pixelWidth);
+        file<<std::setfill('0')<<std::setw(pixelWidth)<<std::hex<<(*it);
     }
 
     file.close();
